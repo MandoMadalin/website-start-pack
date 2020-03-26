87bc93e495b180c839946b31f8bd96713518f5fc
Madalin <31403994+MandoMadalin@users.noreply.github.com>
2020-03-26 10:54:15 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
 
 	
+	$("#mobile-header-action .burger .menu-button i").click(function(){
+		var check_if_open = $(this).parent().hasClass("active");
+		
+		if(check_if_open == true){
+			$(this).removeClass("fa-window-close").addClass("fa-bars");
+			$(this).parent().removeClass("active");
+			$("#mobile-header-menu").slideUp();
+		}else{
+			$(this).addClass("fa-window-close").removeClass("fa-bars");
+			$(this).parent().addClass("active");
+			$("#mobile-header-menu").slideDown();
+		}
+	});
+	
+	
+	
 });

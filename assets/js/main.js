c9452b66a76fa609607db5807634581c157af2e6
Madalin <31403994+MandoMadalin@users.noreply.github.com>
2020-03-31 12:08:36 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,8 +18,21 @@ $(document).ready(function(){
 	
 	
 });
-
 function validateEmail(email) {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
+	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(email);
+}
+
+
+function validatePhone(phone){
+	/*
+	XXXX-XXX-XXX
+	XXXX.XXX.XXX
+	XXXX XXX XXX 
+	*/
+	if(phone.match(/^\(?([0-9]{4})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{3})$/)){
+		return true;
+	}else{
+		return false;
+	}
 }
